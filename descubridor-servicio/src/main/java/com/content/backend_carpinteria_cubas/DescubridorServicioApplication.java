52e128fd25defb4a206678f09b4c1a406a992558
isradev <u23271982@utp.edu.pe>
2025-09-04 17:08:40 -0500

    feat:levantado servidor descubrimiento (Eureka Gateway)

---
+++ descubridor-servicio/src/main/java/com/content/backend_carpinteria_cubas/DescubridorServicioApplication.java
@@ -0,0 +1,15 @@
+package com.content.backend_carpinteria_cubas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DescubridorServicioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DescubridorServicioApplication.class, args);
+    }
+
+}
