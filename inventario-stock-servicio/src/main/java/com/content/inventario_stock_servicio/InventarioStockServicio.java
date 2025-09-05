44a3dba268a7152c64a4ec6bc7bc6a506a590c6b
isradev <u23271982@utp.edu.pe>
2025-09-04 19:51:58 -0500

    feat:levantado inventario stock servicio

---
+++ inventario-stock-servicio/src/main/java/com/content/inventario_stock_servicio/InventarioStockServicio.java
@@ -0,0 +1,11 @@
+package com.content.inventario_stock_servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioStockServicio {
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioStockServicio.class, args);
+    }
+}
