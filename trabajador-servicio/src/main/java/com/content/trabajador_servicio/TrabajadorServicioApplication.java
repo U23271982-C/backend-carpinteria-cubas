de8cc8296e3c551466782aaed298fc242ef2f10c
isradev <u23271982@utp.edu.pe>
2025-09-04 19:40:28 -0500

    feat:levantado servicio trabajador

---
+++ trabajador-servicio/src/main/java/com/content/trabajador_servicio/TrabajadorServicioApplication.java
@@ -0,0 +1,11 @@
+package com.content.trabajador_servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajadorServicioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajadorServicioApplication.class, args);
+    }
+}
