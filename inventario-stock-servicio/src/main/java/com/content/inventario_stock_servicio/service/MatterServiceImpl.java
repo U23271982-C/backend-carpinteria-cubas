9157e5c58e946ba4c11581dd4ce268817dae69af
Chung <chung@gmail.com>
2025-09-17 23:36:48 -0500

    Se implemento el servicio parcialmente del ejemplo

--- inventario-stock-servicio/src/main/java/com/content/inventario_stock_servicio/service/MatterServiceImpl.java
+++ inventario-stock-servicio/src/main/java/com/content/inventario_stock_servicio/service/MatterServiceImpl.java
@@ -5,9 +5,13 @@ import com.content.inventario_stock_servicio.mapper.MatterMapper;
 import com.content.inventario_stock_servicio.repository.MatterRepository;
 import com.content.inventario_stock_servicio.service.abstractService.ServiceAbs;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class MatterServiceImpl implements ServiceAbs<MatterRequestDTO, MatterResponseDTO> {
     private final MatterRepository matter_repository;
     private final MatterMapper matter_mapper;
