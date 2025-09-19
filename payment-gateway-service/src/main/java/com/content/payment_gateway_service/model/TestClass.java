e94ca5dfd5a060f588c3e1d80020f306f1b0fbc7
isradev <u23271982@utp.edu.pe>
2025-09-18 22:10:35 -0500

    clase prueba para que se suban los archivos

---
+++ payment-gateway-service/src/main/java/com/content/payment_gateway_service/model/TestClass.java
@@ -0,0 +1,5 @@
+package com.content.payment_gateway_service.model;
+
+public class TestClass {
+    //Borrar esta clase
+}
---
+++ report-service/src/main/java/com/content/report_service/model/TestClass.java
@@ -0,0 +1,5 @@
+package com.content.report_service.model;
+
+public class TestClass {
+    //Borrar esta clase
+}
