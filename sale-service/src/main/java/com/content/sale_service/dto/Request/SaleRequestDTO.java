dbaa0ea13093662b84f0ca199cd05966595547de
isradev <u23271982@utp.edu.pe>
2025-09-20 18:44:38 -0500

    impl:sale-service dtos

--- sale-service/src/main/java/com/content/sale_service/dto/Request/SaleRequestDTO.java
+++ sale-service/src/main/java/com/content/sale_service/dto/Request/SaleRequestDTO.java
@@ -19,5 +19,7 @@ public class SaleRequestDTO {
     private double subtotal;
     private double total;
     private int client_id;
-    private int state_id;
+    private int state_sale_current_id;
+    private int state_entity_current_id; //
+    private List<SaleDetail> sale_details; // Lista de detalles de la venta, para poder sacar el total y el subtotal
 }
--- sale-service/src/main/java/com/content/sale_service/dto/Response/SaleResponseDTO.java
+++ sale-service/src/main/java/com/content/sale_service/dto/Response/SaleResponseDTO.java
@@ -4,14 +4,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Getter
 @Setter
 @Builder
 public class SaleResponseDTO {
     private String number_sale;
-    private String date_sale;
-    private String state;
-    private int id_client;
-    private int total;
-    private int subtotal;
+    private LocalDate date_sale;
+    private LocalTime hour_sale;
+    private double subtotal;
+    private double total;
+    private int client_id;
+    private String id_state_sale_current;
 }
