1bb280714e97bb9093b97c471e84985aafaa2640
isradev <u23271982@utp.edu.pe>
2025-09-20 18:45:09 -0500

    mapper impl sale-service

--- sale-service/src/main/java/com/content/sale_service/mapper/SaleMapper.java
+++ sale-service/src/main/java/com/content/sale_service/mapper/SaleMapper.java
@@ -5,6 +5,7 @@ import com.content.sale_service.dto.Response.SaleResponseDTO;
 import com.content.sale_service.mapper.convert.Convert;
 import com.content.sale_service.model.Sale;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SaleMapper
@@ -12,6 +13,24 @@ public interface SaleMapper
     @Override
     SaleResponseDTO toDTO(Sale modelo);
 
+    /**
+     * Ignoramos:<br>
+     * id, porque lo pone la bd<br>
+     * number_sale, lo agregamos en el servicio<br>
+     * date_sale, se agrega en el servicio<br>
+     * hour_sale, se agrega en el servicio<br>
+     * subtotal, se agrega en el servicio<br>
+     * total, se agrega en el servicio<br>
+     * @param dto Request DTO
+     * @return Model Sale
+     */
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "number_sale", ignore = true)
+    @Mapping(target = "date_sale", ignore = true)
+    @Mapping(target = "hour_sale", ignore = true)
+    @Mapping(target = "subtotal", ignore = true)
+    @Mapping(target = "total", ignore = true)
+    @Mapping(target = "sale_detail", ignore = true)
     @Override
     Sale toModel(SaleRequestDTO dto);
 }
