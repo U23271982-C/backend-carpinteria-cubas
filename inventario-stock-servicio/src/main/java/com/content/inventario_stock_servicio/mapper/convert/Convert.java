47d052169cc37a2151985633882bf317b5392c48
Chungm7 <chungcoelhove070@gmail.com>
2025-09-21 23:45:45 -0500

    Se comento la interface conver en inventario-servicio

--- inventario-stock-servicio/src/main/java/com/content/inventario_stock_servicio/mapper/convert/Convert.java
+++ inventario-stock-servicio/src/main/java/com/content/inventario_stock_servicio/mapper/convert/Convert.java
@@ -3,8 +3,8 @@ package com.content.inventario_stock_servicio.mapper.convert;
 /**
  *
  * @param <M> modelo
- * @param <DRQ> Request DTO
- * @param <DRE> Response DTO
+ * @param <DRQ> Request DTO (Datos que ingresan al sistema)
+ * @param <DRE> Response DTO (Datos que egresan del sistema)
  */
 public interface Convert <M,DRQ,DRE> extends ConvertModel<M,DRQ>, ConvertDTO<M,DRE>{
 
