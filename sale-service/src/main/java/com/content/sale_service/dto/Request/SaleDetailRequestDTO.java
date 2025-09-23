e91199635dc23d0d3ad739c6b73820c193e9768d
isradev <u23271982@utp.edu.pe>
2025-09-23 10:45:46 -0500

    feat: sale detail request con validaciones

--- sale-service/src/main/java/com/content/sale_service/dto/Request/SaleDetailRequestDTO.java
+++ sale-service/src/main/java/com/content/sale_service/dto/Request/SaleDetailRequestDTO.java
@@ -1,5 +1,7 @@
 package com.content.sale_service.dto.Request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +10,13 @@ import lombok.Setter;
 @Setter
 @Builder
 public class SaleDetailRequestDTO {
+    @NotNull(message = "El id de la venta no puede ser nulo")
+    @Positive(message = "El id de la venta no puede ser negativo")
     private int id_sale;
+    @NotNull(message = "El id del producto no puede ser nulo")
+    @Positive(message = "El id del producto no puede ser negativo")
     private int id_product;
+    @NotNull(message = "La cantidad de productos no puede ser nulo")
+    @Positive(message = "La cantidad de productos no puede ser negativo")
     private int quantity;
 }
