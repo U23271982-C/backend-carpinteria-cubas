528a74a6604d82398aa414b4aae45607871c16cf
isradev <u23271982@utp.edu.pe>
2025-10-01 17:27:13 -0500

    feat:varibles caomunes para formatear

---
+++ sale-service/src/main/java/com/content/sale_service/common/Formatting.java
@@ -0,0 +1,9 @@
+package com.content.sale_service.common;
+
+/**
+ * Clase para definir formatos de datos
+ */
+public class Formatting {
+    public final static String DATE_STANDARD = "ddMMyyyy";
+    public final static String TIME_STANDARD = "HHmmss";
+}
