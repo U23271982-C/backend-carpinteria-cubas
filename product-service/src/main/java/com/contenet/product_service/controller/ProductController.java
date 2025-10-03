84c93ed346a2bbf641a925d29e50ebe228adf5be
isradev <u23271982@utp.edu.pe>
2025-10-03 12:19:17 -0500

    listo product-service

---
+++ product-service/src/main/java/com/contenet/product_service/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.contenet.product_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @GetMapping
+    public String getBien() {
+        return "Se corroboro que funicine product";
+    }
+}
