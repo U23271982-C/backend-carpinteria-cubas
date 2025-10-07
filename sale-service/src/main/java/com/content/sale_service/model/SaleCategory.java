5e8bcb64882ae448e199249bb5d5e0c084140042
Chungm7 <chungcoelhove070@gmail.com>
2025-10-07 15:24:42 -0500

    Modelo sale category implementado en sale service, modelo categorizable de la venta

--- sale-service/src/main/java/com/content/sale_service/model/SaleCategory.java
+++ sale-service/src/main/java/com/content/sale_service/model/SaleCategory.java
@@ -1,4 +1,42 @@
 package com.content.sale_service.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "SaleCategory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SaleCategory {
+    /**
+     * Categoria de venta, para clasificar las ventas en prefabricados y personalizados
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * Nombre de la categoria de venta
+     */
+
+    @Column(name = "sale_category_name", length = 20)
+    private String sale_category_name;
+
+    /**
+     * Estado de la categoria de venta (activo/inactivo/eliminado)
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "state_entity_id", nullable = false)
+    private StateEntity state_entity_id;
+
+    /**
+     * Lista de ventas asociadas a esta categoria
+     */
+    @OneToMany(mappedBy = "sale_category", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Sale> sale;
 }
