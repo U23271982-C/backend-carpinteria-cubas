2b387a4ee1504c48c2642e26f32cee39420d7384
Chungm7 <chungcoelhove070@gmail.com>
2025-10-12 13:09:16 -0500

    feat: Repository de state entity para interactuar con la db

--- authentication-service/src/main/java/com/content/authentication_service/repository/StateEntityRepository.java
+++ authentication-service/src/main/java/com/content/authentication_service/repository/StateEntityRepository.java
@@ -2,6 +2,8 @@ package com.content.authentication_service.repository;
 
 import com.content.authentication_service.model.StateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateEntityRepository extends JpaRepository<StateEntity,Integer> {
 }
