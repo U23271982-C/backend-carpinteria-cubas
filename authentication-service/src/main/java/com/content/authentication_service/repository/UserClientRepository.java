afd6389e097f1925e8e972f687da8656f78c8cb6
Chungm7 <chungcoelhove070@gmail.com>
2025-10-12 13:09:51 -0500

    feat: Repository de user client para interactuar con la db y con admin sdk

--- authentication-service/src/main/java/com/content/authentication_service/repository/UserClientRepository.java
+++ authentication-service/src/main/java/com/content/authentication_service/repository/UserClientRepository.java
@@ -3,5 +3,8 @@ package com.content.authentication_service.repository;
 import com.content.authentication_service.model.UserClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserClientRepository extends JpaRepository<UserClient,Integer> {
+    Optional<UserClient> findByFireBaseUid(String fireBaseUid);
 }
