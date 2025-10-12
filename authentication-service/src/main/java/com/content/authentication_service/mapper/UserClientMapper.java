73d6861bb3b07212a02cd6ad1de9cb0aa4fed7f4
Chungm7 <chungcoelhove070@gmail.com>
2025-10-12 13:10:39 -0500

    feat: Mapper hecho de manera empirica podria necesitarse mejorar logica

--- authentication-service/src/main/java/com/content/authentication_service/mapper/UserClientMapper.java
+++ authentication-service/src/main/java/com/content/authentication_service/mapper/UserClientMapper.java
@@ -1,14 +1,17 @@
 package com.content.authentication_service.mapper;
 
+
 import com.content.authentication_service.dto.request.UserClientRequestDTO;
 import com.content.authentication_service.dto.response.UserClientResponseDTO;
 import com.content.authentication_service.mapper.convert.Convert;
 import com.content.authentication_service.model.UserClient;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-public interface UserClientMapper
-        extends Convert<UserClient, UserClientRequestDTO, UserClientResponseDTO> {
+@Mapper(componentModel = "spring")
+public interface UserClientMapper extends Convert<UserClient, UserClientRequestDTO, UserClientResponseDTO> {
+
     @Override
     @Mappings({
             // Mapea el campo `state_entity_name` del objeto anidado a `state` en el DTO.
@@ -16,7 +19,8 @@ public interface UserClientMapper
             @Mapping(source = "user_client_email", target = "email"),
             @Mapping(source = "user_client_full_name", target = "fullName"),
             @Mapping(source = "user_client_phone", target = "phone"),
-            @Mapping(source = "user_client_address", target = "address")
+            @Mapping(source = "user_client_address", target = "address"),
+            @Mapping(source = "user_client_created_at", target = "createdAt")
     })
     UserClientResponseDTO toDTO(UserClient model);
 
@@ -24,12 +28,13 @@ public interface UserClientMapper
     @Mappings({
             // Ignoramos campos que se manejarán en la capa de servicio.
             @Mapping(target = "user_client_id", ignore = true),
-            @Mapping(target = "fire_base_uid", ignore = true),
+            @Mapping(target = "fireBaseUid", ignore = true), // <-- CORREGIDO: Usar camelCase
             @Mapping(target = "state_entity_id", ignore = true), // El servicio se encargará de asignar el estado.
             @Mapping(source = "email", target = "user_client_email"),
             @Mapping(source = "fullName", target = "user_client_full_name"),
             @Mapping(source = "phone", target = "user_client_phone"),
-            @Mapping(source = "address", target = "user_client_address")
+            @Mapping(source = "address", target = "user_client_address"),
+            @Mapping(target = "user_client_created_at", ignore = true) // Se asigna en el servicio
     })
     UserClient toModel(UserClientRequestDTO dto);
 }
