a36b50c119fbbe0473d3efb3baa7123289f1475b
Chungm7 <chungcoelhove070@gmail.com>
2025-10-13 00:47:37 -0500

    feat: Repository optimizado

--- authentication-service/src/main/java/com/content/authentication_service/repository/UserClientRepository.java
+++ authentication-service/src/main/java/com/content/authentication_service/repository/UserClientRepository.java
@@ -3,8 +3,5 @@ package com.content.authentication_service.repository;
 import com.content.authentication_service.model.UserClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserClientRepository extends JpaRepository<UserClient,Integer> {
-    Optional<UserClient> findByFireBaseUid(String fireBaseUid);
 }
