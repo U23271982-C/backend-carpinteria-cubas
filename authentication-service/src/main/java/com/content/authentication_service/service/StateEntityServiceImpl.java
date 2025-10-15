7d329eca1b9b42bd1c40ca480fb278c45f2241ed
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 14:50:11 -0500

    feat: Se implemento netamente el listar ya que esta entidad maneja datos inmutables

--- authentication-service/src/main/java/com/content/authentication_service/service/StateEntityServiceImpl.java
+++ authentication-service/src/main/java/com/content/authentication_service/service/StateEntityServiceImpl.java
@@ -1,4 +1,50 @@
 package com.content.authentication_service.service;
 
-public class StateEntityServiceImpl {
+import com.content.authentication_service.dto.request.StateEntityRequestDTO;
+import com.content.authentication_service.dto.response.StateEntityResponseDTO;
+import com.content.authentication_service.mapper.StateEntityMapper;
+import com.content.authentication_service.repository.StateEntityRepository;
+import com.content.authentication_service.service.abstractservice.ServiceAbs;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class StateEntityServiceImpl implements ServiceAbs<StateEntityRequestDTO, StateEntityResponseDTO> {
+
+    private final StateEntityRepository stateEntityRepository;
+    private final StateEntityMapper stateEntityMapper;
+
+    @Override
+    public StateEntityResponseDTO create(StateEntityRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public List<StateEntityResponseDTO> allList() {
+        return stateEntityRepository. findAll()
+                .stream()
+                .map(stateEntityMapper::toDTO)
+                .toList();
+    }
+
+    @Override
+    public StateEntityResponseDTO readById(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public void remove(UUID uuid) {
+
+    }
+
+    @Override
+    public StateEntityResponseDTO update(UUID uuid, StateEntityRequestDTO dto) {
+        return null;
+    }
 }
