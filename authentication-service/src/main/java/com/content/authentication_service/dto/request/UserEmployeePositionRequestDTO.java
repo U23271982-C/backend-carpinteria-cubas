7d52a29d873c82af89ee3606f235bf56b152ceac
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 14:43:55 -0500

    feat: Se implemento el dtorequest de la entidad positiouseremployee

--- authentication-service/src/main/java/com/content/authentication_service/dto/request/UserEmployeePositionRequestDTO.java
+++ authentication-service/src/main/java/com/content/authentication_service/dto/request/UserEmployeePositionRequestDTO.java
@@ -1,15 +1,23 @@
 package com.content.authentication_service.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @Builder
-public class UserEmployeePostRequestDTO {
+public class UserEmployeePositionRequestDTO {
 
+    @NotEmpty(message = "El nombre del puesto es obligatorio")
     private String positionName;
+
+    @NotBlank(message = "La descripción del puesto es obligatoria")
     private String positionDescription;
+
     private UUID stateEntityuuid;
 }
