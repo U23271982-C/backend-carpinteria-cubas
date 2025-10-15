5cba85f23557bcffeffd11ccc838ddec3174d7a0
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 14:46:10 -0500

    feat: Se realizaron los dto para mostrar en el front de stateentity

--- authentication-service/src/main/java/com/content/authentication_service/dto/response/StateEntityResponseDTO.java
+++ authentication-service/src/main/java/com/content/authentication_service/dto/response/StateEntityResponseDTO.java
@@ -1,4 +1,14 @@
 package com.content.authentication_service.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@Getter
+@Setter
+@Builder
 public class StateEntityResponseDTO {
+    private UUID uuid;
+    private String name;
 }
