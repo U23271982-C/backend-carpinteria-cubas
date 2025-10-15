e11a2ede7d2b2a294b1b8d89a2512db0c4f1794c
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 14:48:42 -0500

    feat: Se implementaron los mapeos de dto reposnse y request

--- authentication-service/src/main/java/com/content/authentication_service/mapper/UserEmployeePositionMapper.java
+++ authentication-service/src/main/java/com/content/authentication_service/mapper/UserEmployeePositionMapper.java
@@ -1,12 +1,30 @@
 package com.content.authentication_service.mapper;
 import com.content.authentication_service.dto.request.UserEmployeePositionRequestDTO;
+import com.content.authentication_service.dto.response.UserEmployeePositionResponseDTO;
 import com.content.authentication_service.mapper.convert.Convert;
+import com.content.authentication_service.model.UserEmployeePosition;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface UserEmployeePositionMapper extends Convert<UserEmployeePosition, UserEmployeePositionRequestDTO, UserEmployeePositionRequestDTO > {
+public interface UserEmployeePositionMapper extends Convert<UserEmployeePosition, UserEmployeePositionRequestDTO, UserEmployeePositionResponseDTO > {
 
-    U
+    UserEmployeePositionMapper INSTANCE = org.mapstruct.factory.Mappers.getMapper(UserEmployeePositionMapper.class);
+
+    @Mapping (source = "uuid", target = "uuid")
+    @Mapping (source = "position_name", target = "positionName")
+    @Mapping(source = "position_description", target = "positionDescription")
+    @Mapping(source = "state_entity_id.state_entity_name", target = "nameStateEntity")
+    @Override
+    UserEmployeePositionResponseDTO toDTO(UserEmployeePosition modelo);
+
+    @Mapping(target = "user_employee_position_id", ignore = true)
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "position_name", source = "positionName")
+    @Mapping(target = "position_description", source = "positionDescription")
+    @Mapping(target = "state_entity_id.uuid", source = "stateEntityuuid") // Asignamos el UUID del estado directamente
+    @Override
+    UserEmployeePosition toModel(UserEmployeePositionRequestDTO dto);
+
 
 }
