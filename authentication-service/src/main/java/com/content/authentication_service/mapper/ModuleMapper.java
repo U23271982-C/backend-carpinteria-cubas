a38d9d75326ef89f87617477214611ebd1d974ce
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 18:33:59 -0500

    feat: Mapeo de dtos y modelos

--- authentication-service/src/main/java/com/content/authentication_service/mapper/ModuleMapper.java
+++ authentication-service/src/main/java/com/content/authentication_service/mapper/ModuleMapper.java
@@ -1,4 +1,30 @@
 package com.content.authentication_service.mapper;
+import com.content.authentication_service.dto.request.ModuleRequestDTO;
+import com.content.authentication_service.dto.response.ModuleResponseDTO;
+import com.content.authentication_service.mapper.convert.Convert;
+import com.content.authentication_service.model.Module;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-public interface ModuleMapper {
+@Mapper(componentModel = "spring")
+public interface ModuleMapper extends Convert<Module, ModuleRequestDTO, ModuleResponseDTO> {
+
+    ModuleMapper INSTANCE = org.mapstruct.factory.Mappers.getMapper(ModuleMapper.class);
+
+    @Mapping(source = "uuid", target = "uuid")
+    @Mapping(source = "module_name", target = "module_name")
+    @Mapping(source = "module_description", target = "module_description")
+    @Mapping(source = "state_entity_id.state_entity_name", target = "state_entity_name")
+    @Override
+    ModuleResponseDTO toDTO(Module modelo);
+
+
+    @Mapping(target = "module_id", ignore = true)
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "module_name", source = "module_name")
+    @Mapping(target = "module_description", source = "module_description")
+    @Mapping(target = "state_entity_id.uuid", source = "stateEntityuuid") // Asignamos el UUID del estado directamente
+    @Override
+    Module toModel(ModuleRequestDTO dto);
+
 }
