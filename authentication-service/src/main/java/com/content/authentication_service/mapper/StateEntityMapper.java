0c043868ab47abd4d226dfd1602b8847fe4d74f9
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 14:48:10 -0500

    feat: Se implemento solo el mappeo del response

--- authentication-service/src/main/java/com/content/authentication_service/mapper/StateEntityMapper.java
+++ authentication-service/src/main/java/com/content/authentication_service/mapper/StateEntityMapper.java
@@ -1,4 +1,25 @@
 package com.content.authentication_service.mapper;
 
-public interface StateEntity {
+import com.content.authentication_service.dto.request.UserClientRequestDTO;
+import com.content.authentication_service.dto.response.StateEntityResponseDTO;
+import com.content.authentication_service.mapper.convert.Convert;
+import com.content.authentication_service.model.StateEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface StateEntityMapper extends Convert<StateEntity,UserClientRequestDTO, StateEntityResponseDTO> {
+
+    StateEntityMapper INSTANCE = org.mapstruct.factory.Mappers.getMapper(StateEntityMapper.class);
+
+
+    @Mapping(source = "uuid", target = "uuid")
+    @Mapping(source = "state_entity_name", target = "name")
+    @Override
+    StateEntityResponseDTO toDTO(StateEntity modelo);
+
+
+
+    @Override
+    StateEntity toModel(UserClientRequestDTO dto);
 }
