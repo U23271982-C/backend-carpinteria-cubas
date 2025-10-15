ee9c29141841decd84b97c89141b51157f9e8e62
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 18:33:25 -0500

    feat: Datos de entrada listo

--- authentication-service/src/main/java/com/content/authentication_service/dto/request/ModuleRequestDTO.java
+++ authentication-service/src/main/java/com/content/authentication_service/dto/request/ModuleRequestDTO.java
@@ -1,11 +1,22 @@
 package com.content.authentication_service.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @Builder
 public class ModuleRequestDTO {
+
+    @NotBlank(message = "El nombre del módulo es obligatorio")
+    private String module_name;
+
+    @NotBlank(message = "La descripción del módulo es obligatoria")
+    private String module_description;
+
+    private UUID stateEntityuuid;
 }
