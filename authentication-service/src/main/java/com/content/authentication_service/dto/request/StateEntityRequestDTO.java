676c55d011f173423197351e0be2cac24c201621
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 14:45:30 -0500

    feat: Se implemento el StateEntityRequesDTO, que al final no tendra uso ya que deben ser datos inmuÂtables los registros de estadoentidad

--- authentication-service/src/main/java/com/content/authentication_service/dto/request/StateEntityRequestDTO.java
+++ authentication-service/src/main/java/com/content/authentication_service/dto/request/StateEntityRequestDTO.java
@@ -1,4 +1,11 @@
 package com.content.authentication_service.dto.request;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class StateEntityRequestDTO {
 }
