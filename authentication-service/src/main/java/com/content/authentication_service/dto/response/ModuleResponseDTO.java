b1db3c210789c5888a92ed9e522596a8f6052dc2
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 18:33:42 -0500

    feat: Datos de salida finalizados

--- authentication-service/src/main/java/com/content/authentication_service/dto/response/ModuleResponseDTO.java
+++ authentication-service/src/main/java/com/content/authentication_service/dto/response/ModuleResponseDTO.java
@@ -4,8 +4,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @Builder
 public class ModuleResponseDTO {
+
+    private UUID uuid;
+
+    private String module_name;
+
+    private String module_description;
+
+    private String state_entity_name;
 }
