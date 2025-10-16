4d1d212ec339f84b1917dffe8b20abdf2bb394eb
isradev <u23271982@utp.edu.pe>
2025-10-16 13:21:10 -0500

    listo post

--- employee-service/src/main/java/com/content/employee_service/dto/request/PostRequestDTO.java
+++ employee-service/src/main/java/com/content/employee_service/dto/request/PostRequestDTO.java
@@ -1,8 +1,11 @@
 package com.content.employee_service.dto.request;
 
+import com.content.employee_service.utility.ValidateGroup;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.UUID;
 
 /**
  * Data Transfer Object para la entidad Position.
@@ -15,13 +18,14 @@ import lombok.*;
 public class PostRequestDTO {
 
     // Nombre del cargo
-    @Size(min = 1, max = 100, message = "El nombre del cargo debe tener entre 1 y 100 caracteres")
-    @NotBlank(message = "El nombre del cargo no puede estar en blanco")
+    @Size(min = 1, max = 100, message = "El nombre del cargo debe tener entre 1 y 100 caracteres", groups = {ValidateGroup.OnCreate.class, ValidateGroup.OnUpdate.class})
+    @NotBlank(message = "El nombre del cargo no puede estar en blanco", groups = ValidateGroup.OnCreate.class)
     private String post_name;
 
     // Descripción del cargo
-    @Size(min = 1, max = 250, message = "La descripción debe tener entre 1 y 250 caracteres")
-    @NotBlank(message = "La descripción no puede estar en blanco")
+    @Size(min = 1, max = 250, message = "La descripción debe tener entre 1 y 250 caracteres", groups = {ValidateGroup.OnCreate.class, ValidateGroup.OnUpdate.class})
+    @NotBlank(message = "La descripción no puede estar en blanco", groups = ValidateGroup.OnCreate.class)
     private String description;
 
+    private UUID state_entity_uuid;
 }
--- employee-service/src/main/java/com/content/employee_service/dto/response/PostResponseDTO.java
+++ employee-service/src/main/java/com/content/employee_service/dto/response/PostResponseDTO.java
@@ -3,9 +3,13 @@ package com.content.employee_service.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Builder
 @Data
 public class PostResponseDTO {
+    private UUID uuid;
     private String post_name;
-    private int description;
+    private String description;
+    private String state_entity;
 }
