d7f1886539416eeb2eb9562f70b67ffd6aca56c4
Chungm7 <chungcoelhove070@gmail.com>
2025-10-16 14:05:00 -0500

    feat: Se comento la logica de asginacion para estado default

--- authentication-service/src/main/java/com/content/authentication_service/service/UserEmployeePositionServiceImpl.java
+++ authentication-service/src/main/java/com/content/authentication_service/service/UserEmployeePositionServiceImpl.java
@@ -34,13 +34,20 @@ public class UserEmployeePositionServiceImpl implements ServiceAbs<UserEmployeeP
         userEmployeePosition.setUuid(uuid);
 
         if (userEmployeePosition.getState_entity_id().getUuid() == null) {
+            /**
+             * El DTO-Request que tenemos recibe comom parametro el
+             * @Param UUID del estado, pero en si no se proporciona
+             * Pero nosotros en esta capa supongo que estas asignadole direcamente al objeto StateEntity(1) o sea activo
+             * pero estas actualizando directamente el parametro, por lo que en el response no esta trayendo el nombre del estado
+             * pero internamente esta con estado 1 pero no en el return el mapper solo lleva el id que actualizaste y no puede convertirlo
+             * por lo que retorna null a pesar de realizar el cambio de estado correctamente
+             */
             log.info("No se proporcionó state_entity_id, asignando estado predeterminado (activo)");
             StateEntity defaultState = stateEntityRepository.findAll()
                     .stream()
                     .filter(stateEntity -> stateEntity.getState_entity_id() == 1)
                     .findFirst()
                     .orElseThrow(() -> new RuntimeException("No se encontro state_entity"));
-
             userEmployeePosition.setState_entity_id(defaultState);
         }
 
