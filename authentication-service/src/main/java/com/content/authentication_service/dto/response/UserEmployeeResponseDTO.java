e539bab546f84ceea89ed2b2d672706e258d3874
Chungm7 <chungcoelhove070@gmail.com>
2025-10-16 00:09:11 -0500

    feat: Se implemento los datos de salida de useremployee

--- authentication-service/src/main/java/com/content/authentication_service/dto/response/UserEmployeeResponseDTO.java
+++ authentication-service/src/main/java/com/content/authentication_service/dto/response/UserEmployeeResponseDTO.java
@@ -1,4 +1,20 @@
 package com.content.authentication_service.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
 public class UserEmployeeResponseDTO {
+
+    private UUID uuid;
+    private String name;
+    private String position;
+    private String phone;
+    private String state;
+
 }
