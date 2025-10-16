36d0830ac4831a32d5dd5820f9f07355b1cdfbaa
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 19:42:15 -0500

    feat: Se implemento logica de negocio para ModeloAction

--- authentication-service/src/main/java/com/content/authentication_service/service/ActionServiceImpl.java
+++ authentication-service/src/main/java/com/content/authentication_service/service/ActionServiceImpl.java
@@ -1,4 +1,50 @@
 package com.content.authentication_service.service;
 
-public class ActionServiceImpl {
+import com.content.authentication_service.dto.request.ActionRequestDTO;
+import com.content.authentication_service.dto.response.ActionResponseDTO;
+import com.content.authentication_service.mapper.ActionMapper;
+import com.content.authentication_service.repository.ActionRepository;
+import com.content.authentication_service.service.abstractservice.ServiceAbs;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ActionServiceImpl implements ServiceAbs<ActionRequestDTO, ActionResponseDTO> {
+
+    private final ActionRepository actionRepository;
+    private final ActionMapper actionMapper;
+
+    @Override
+    public ActionResponseDTO create(ActionRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    public List<ActionResponseDTO> allList() {
+        return actionRepository.findAll()
+                .stream()
+                .filter(action -> action.getState_entity_id().getState_entity_id() != 3)
+                .map(actionMapper::toDTO)
+                .toList(); // Excluir eliminados;
+    }
+
+    @Override
+    public ActionResponseDTO readById(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public void remove(UUID uuid) {
+
+    }
+
+    @Override
+    public ActionResponseDTO update(UUID uuid, ActionRequestDTO dto) {
+        return null;
+    }
 }
