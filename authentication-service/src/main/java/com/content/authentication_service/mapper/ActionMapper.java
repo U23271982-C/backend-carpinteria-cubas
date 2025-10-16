5c850c60572400865190889009b8968b827d7ab1
Chungm7 <chungcoelhove070@gmail.com>
2025-10-15 19:43:52 -0500

    feat: Se implmeento el mapeo de convert

--- authentication-service/src/main/java/com/content/authentication_service/mapper/ActionMapper.java
+++ authentication-service/src/main/java/com/content/authentication_service/mapper/ActionMapper.java
@@ -1,4 +1,23 @@
 package com.content.authentication_service.mapper;
 
-public interface ActionMapper {
+import com.content.authentication_service.dto.request.ActionRequestDTO;
+import com.content.authentication_service.dto.response.ActionResponseDTO;
+import com.content.authentication_service.mapper.convert.Convert;
+import com.content.authentication_service.model.Action;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ActionMapper extends Convert<Action, ActionRequestDTO, ActionResponseDTO> {
+
+
+    @Mapping(source = "uuid", target= "uuid")
+    @Mapping(source = "action_name", target= "action_name")
+    @Mapping(source = "action_description", target= "action_description")
+    @Override
+    ActionResponseDTO toDTO(Action modelo);
+
+    @Override
+    Action toModel(ActionRequestDTO dto);
+
 }
