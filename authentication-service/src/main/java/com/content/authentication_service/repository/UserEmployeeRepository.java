4f0b0755ef39876d1945151306ffdde8546c03a2
Chungm7 <chungcoelhove070@gmail.com>
2025-10-16 00:10:04 -0500

    feat: Repopsitory JPA de UserEmployee realizado

--- authentication-service/src/main/java/com/content/authentication_service/repository/UserEmployeeRepository.java
+++ authentication-service/src/main/java/com/content/authentication_service/repository/UserEmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.content.authentication_service.repository;
 
-public interface EmployeeRepository {
+import com.content.authentication_service.model.UserEmployee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserEmployeeRepository extends JpaRepository<UserEmployee, Integer> {
 }
