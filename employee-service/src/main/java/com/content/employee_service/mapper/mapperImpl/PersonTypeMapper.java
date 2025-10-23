b0c069e1c92990107e3e866a3f10fba2bd2841e2
isradev <u23271982@utp.edu.pe>
2025-10-23 18:04:05 -0500

    fix-add:updatePatch PersonTypeMapper

--- employee-service/src/main/java/com/content/employee_service/mapper/mapperImpl/PersonTypeMapper.java
+++ employee-service/src/main/java/com/content/employee_service/mapper/mapperImpl/PersonTypeMapper.java
@@ -3,13 +3,15 @@ package com.content.employee_service.mapper.mapperImpl;
 import com.content.employee_service.dto.request.PersonTypeRequestDTO;
 import com.content.employee_service.dto.response.PersonTypeResponseDTO;
 import com.content.employee_service.mapper.convert.Convert;
+import com.content.employee_service.mapper.convert.UpdatePatch;
 import com.content.employee_service.model.PersonType;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PersonTypeMapper
-        extends Convert<PersonType, PersonTypeRequestDTO, PersonTypeResponseDTO> {
+        extends Convert<PersonType, PersonTypeRequestDTO, PersonTypeResponseDTO>,
+        UpdatePatch<PersonTypeRequestDTO, PersonType> {
     @Mapping(target = "person_type_name", source = "person_type_name")
     @Mapping(target = "uuid", source = "uuid")
     @Mapping(target = "state_entity", source = "state_entity_id.state_entity_name")
