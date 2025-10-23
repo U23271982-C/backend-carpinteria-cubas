12d34cc0f189ce15e8e02ea00cc43eb75080c886
isradev <u23271982@utp.edu.pe>
2025-10-23 11:28:11 -0500

    fix: validations group in personTypeRequestDTO

--- employee-service/src/main/java/com/content/employee_service/dto/request/PersonTypeRequestDTO.java
+++ employee-service/src/main/java/com/content/employee_service/dto/request/PersonTypeRequestDTO.java
@@ -1,5 +1,6 @@
 package com.content.employee_service.dto.request;
 
+import com.content.employee_service.utility.ValidateGroup;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -17,8 +18,8 @@ import java.util.UUID;
 public class PersonTypeRequestDTO {
 
     // Nombre del tipo de persona
-    @Size(min = 1, max = 100)
-    @NotBlank(message = "El nombre del tipo de persona no puede estar en blanco")
+    @Size(min = 1, max = 100, message = "El nombre del tipo de persona tiene que estar entre 1 a 100 caracteres", groups = {ValidateGroup.OnCreate.class, ValidateGroup.OnUpdate.class})
+    @NotBlank(message = "El nombre del tipo de persona no puede estar en blanco", groups = ValidateGroup.OnCreate.class)
     private String person_type_name;
 
     private UUID state_entity_uuid;
